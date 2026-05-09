00646e320ac5faab5119fa231efac55ce3390ca3
Sergey Shirobokov <shirobokovsv1@develop.mos.ru>
2026-05-09 17:49:54 +0300

    выполнено задание 3"

--- 03/src/locals.tf
+++ 03/src/locals.tf
@@ -2,13 +2,5 @@ locals {
   vm_name        = "web"
   each_vm_config = { for vm in var.each_vm : vm.vm_name => vm }
   ssh_key        = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
-  # storage_data = try(
-  #   {
-  #     name       = yandex_compute_instance.storage.name,
-  #     ip_address = yandex_compute_instance.storage.network_interface[0].nat_ip_address,
-  #     fqdn       = yandex_compute_instance.storage.fqdn,
-  #   },
-  #   null
-  # )
 }
 
