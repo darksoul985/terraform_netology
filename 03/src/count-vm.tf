0d6af3418fe6a87fb0b80ad58ff10ad8cfa81222
Sergey Shirobokov <shirobokovsv1@develop.mos.ru>
2026-05-11 14:41:19 +0300

    добавлен gitignore в папке 01, outputs.tf в папке 02

---
+++ 03/src/count-vm.tf
@@ -0,0 +1,34 @@
+data "yandex_compute_image" "ubuntu" {
+  family = var.web_os_image
+}
+
+resource "yandex_compute_instance" "web" {
+  depends_on = [yandex_compute_instance.database]
+  count      = 2
+  name       = "${local.vm_name}-${count.index + 1}"
+  resources {
+    cores         = var.vpc_resources.web.cores
+    memory        = var.vpc_resources.web.memory
+    core_fraction = var.vpc_resources.web.core_fraction
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+      size     = var.vpc_resources.web.hdd_size
+      type     = var.vpc_resources.web.hdd_type
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.develop.id
+    nat                = true
+    security_group_ids = [yandex_vpc_security_group.example.id]
+  }
+
+  metadata = {
+    serial-port-enable = 1
+    ssh-keys           = local.ssh_key
+  }
+}
